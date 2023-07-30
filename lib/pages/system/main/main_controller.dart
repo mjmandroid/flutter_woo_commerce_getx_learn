885e46e09b96b09b41dbb61ac48b03ddb8102f95
mjm <123@163.com>
2023-07-30 23:43:52 +0800

    [业务模块初始]

---
+++ lib/pages/system/main/main_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+
+import 'main_state.dart';
+
+class MainController extends GetxController {
+  final MainState state = MainState();
+}
---
+++ lib/pages/system/main/main_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'main_controller.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return const _MainViewGetX();
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
+
+/// getx 组件
+class _MainViewGetX extends GetView<MainController> {
+  const _MainViewGetX({Key? key}) : super(key: key);
+
+  Widget _buildView() {
+    return const Center(
+      child: Text("MainPage"),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<MainController>(
+      init: MainController(),
+      id: "main",
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(title: const Text("main")),
+          body: SafeArea(
+            child: _buildView(),
+          ),
+        );
+      },
+    );
+  }
+}
